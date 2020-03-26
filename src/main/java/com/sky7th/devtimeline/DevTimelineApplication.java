ef583502bdb29f8074bc2a3fa1b2099493d9c37b
sky7th <xoghk0321@gmail.com>
2020-03-26 22:03:38 +0900

    Initial commit

---
+++ src/main/java/com/sky7th/devtimeline/DevTimelineApplication.java
@@ -0,0 +1,13 @@
+package com.sky7th.devtimeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTimelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTimelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky7th/devtimeline/DevTimelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky7th.devtimeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTimelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
