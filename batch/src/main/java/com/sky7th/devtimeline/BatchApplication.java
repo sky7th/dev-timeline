7c47515182ecc1c029dca19faebc826c65f83e0e
sky7th <xoghk0321@gmail.com>
2020-03-27 12:05:05 +0900

    Add 멀티 모듈 설정

--- batch/src/main/java/com/sky7th/devtimeline/BatchApplication.java
+++ batch/src/main/java/com/sky7th/devtimeline/BatchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevTimelineApplication {
+public class BatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevTimelineApplication.class, args);
+		SpringApplication.run(BatchApplication.class, args);
 	}
 
 }
