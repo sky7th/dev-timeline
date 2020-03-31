7a20e20f565bda3634cc4def80dc084422a6e515
sky7th <xoghk0321@gmail.com>
2020-03-31 12:51:13 +0900

    Add crawling scheduler / 사용하지 않는 dto 제거

---
+++ batch/src/main/java/com/sky7th/devtimeline/batch/scheduler/Scheduler.java
@@ -0,0 +1,25 @@
+package com.sky7th.devtimeline.batch.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Date;
+
+@Configuration
+@RequiredArgsConstructor
+public class Scheduler {
+
+    private final Job job;
+    private final JobLauncher jobLauncher;
+
+    @Scheduled(cron = "0 0 0/6 * * ?")
+    public void crawlingBatch() throws Exception {
+        jobLauncher.run(job, new JobParametersBuilder()
+                .addDate("requestDate", new Date()).toJobParameters());
+    }
+
+}
