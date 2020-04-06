90a1f7ab343f317a3b20a6fb855bb7f1a479a77b
sky7th <xoghk0321@gmail.com>
2020-04-06 15:38:54 +0900

    Add 체크박스 클릭할 때마다 선택한 기업들의 채용 정보 목록 가져옴

--- web/src/front/src/components/Content.vue
+++ web/src/front/src/components/Content.vue
@@ -1,15 +1,63 @@
 <template>
   <div class="content">
-    <Recruit/>
+    <!-- <Recruit/> -->
+    <div class="company-list">
+      <ul>        
+        <li>
+          <input type="checkbox" id="naver" value="NAVER" 
+            v-model="checkedCompanies"
+          >
+          <label for="Naver">Naver</label>
+        </li>
+        <li>
+          <input type="checkbox" id="kakao" value="KAKAO" 
+            v-model="checkedCompanies"
+          >
+          <label for="kakao">Kakao</label>
+        </li>
+      </ul>
+    </div>
+    <div class="post-list">
+      <ul>
+        <li
+          v-for="({
+            id, 
+            companyTypeName, 
+            companyUrlTypeName, 
+            title,
+            closingDate }) in posts"
+          :key="id"
+        >
+          <div>{{ companyTypeName }}</div>
+          <div>{{ companyUrlTypeName }}</div>
+          <div>{{ title }}</div>
+          <div>{{ closingDate }}</div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import Recruit from '@/components/content/Recruit';
+// import Recruit from '@/components/content/Recruit';
 
 export default {
-  components: {
-    Recruit
+  // components: {
+  //   Recruit
+  // }
+  props: {
+    posts: { type: Array, default: () => [] }
+  },
+  data() {
+    return {
+      checkedCompanies: []
+    }
+  },
+  watch: {
+    checkedCompanies() {
+      this.$emit('updateCompanies', this.checkedCompanies)
+      this.$emit('updatePosts')
+    }
   }
 }
 </script>
@@ -23,5 +71,17 @@ export default {
   margin-top: 50px;
   animation: fadeIn 0.3s ease-in-out;
   z-index: 900;
+}
+.post-list {
+  padding: 20px 20px;
+}
+.company-list {
+  border-bottom: 1px solid black;
+}
+.company-list ul {
+  display: flex;
+}
+.company-list li {
+  margin: 15px 15px;
 }
 </style>
--- web/src/front/src/views/Home.vue
+++ web/src/front/src/views/Home.vue
@@ -3,21 +3,86 @@
     <Header/>
     <div class="wrap">
       <SideMenu/>
-      <Content/>
+      <Content
+        :posts="posts"
+        @updatePosts="updatePosts"
+        @updateCompanies="updateCompanies"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 import Header from '@/components/Header';
 import Content from '@/components/Content';
 import SideMenu from '@/components/SideMenu';
+
+var getCompaniesQuery = (companies) => {
+  var companiesQuery = '';
+  companies.forEach(company => {
+    companiesQuery += '&companies='+company
+  });
+  return companiesQuery;
+}
+
+var getTitleQuery = (title) => '&title=' + title;
+var getOffsetQuery = (offset) => '&offset=' + offset;
 
 export default {
   components: {
     Header,
     Content,
     SideMenu
+  },
+  data: () => {
+    return {
+      posts: [],
+      companies: [],
+      offset: 0
+    }
+  },
+  created () { 
+    axios.get('http://127.0.0.1:8080/api/v1/recruit-posts?'
+              + getOffsetQuery(this.offset))
+      .then(response => {
+        this.posts = response.data.data.recruitPosts
+        this.offset = response.data.data.offset
+      })
+      .catch(e => {
+        console.log('error : ', e)
+      })
+  },
+  methods: {
+    updatePosts() {
+      axios.get('http://127.0.0.1:8080/api/v1/recruit-posts?'
+                + getCompaniesQuery(this.companies))
+      .then(response => {
+        console.log(response)
+        this.posts = response.data.data.recruitPosts
+        this.offset = response.data.data.offset
+      })
+      .catch(e => {
+        console.log('error : ', e)
+      })
+    },
+    updatePostsByTitle(title) {
+      axios.get('http://127.0.0.1:8080/api/v1/recruit-posts?'
+                + getOffsetQuery(this.offset)
+                + getCompaniesQuery(this.companies)
+                + getTitleQuery(title))
+      .then(response => {
+        this.posts = response.data.data.recruitPosts
+        this.offset = response.data.data.offset
+      })
+      .catch(e => {
+        console.log('error : ', e)
+      })
+    },
+    updateCompanies(companies) {
+      this.companies = companies
+    }
   }
 };
 </script>
