fb4c8777508dec8383bb76ef3d80fd4e88086599
sky7th <xoghk0321@gmail.com>
2020-04-11 19:55:13 +0900

    update cors 설정 변경

--- web/src/main/java/com/sky7th/devtimeline/web/config/WebMvcConfigImpl.java
+++ web/src/main/java/com/sky7th/devtimeline/web/config/WebMvcConfigImpl.java
@@ -7,10 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfigImpl implements WebMvcConfigurer {
 
+    private final long MAX_AGE_SECS = 3600;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
     }
 
 }
