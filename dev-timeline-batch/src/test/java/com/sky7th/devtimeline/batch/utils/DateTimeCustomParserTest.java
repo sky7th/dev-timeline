9c3cbf535a2f88f40d0eb730287c516cae751e81
Taehwa Lee <xoghk0321@gmail.com>
2020-09-21 23:08:11 +0900

    feat: batch 모듈 github action 자동 배포

--- dev-timeline-batch/src/test/java/com/sky7th/devtimeline/batch/utils/DateTimeCustomParserTest.java
+++ dev-timeline-batch/src/test/java/com/sky7th/devtimeline/batch/utils/DateTimeCustomParserTest.java
@@ -1,16 +1,16 @@
-package com.sky7th.devtimeline.batch.utils;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-class DateTimeCustomParserTest {
-
-    @DisplayName("문자열 날짜에서 한글과 양 옆 공백을 제거한다.")
-    @Test
-    void removeKorean() {
-        System.out.print(DateTimeCustomParser.parseClosingTime("2020.05.19~2020.08.31"));
-        assertThat(DateTimeCustomParser.parseClosingTime("2020년 6월 6일 까지")).isEqualTo("2020-06-06 00:00:00");
-    }
-}
+//package com.sky7th.devtimeline.batch.utils;
+//
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//class DateTimeCustomParserTest {
+//
+//    @DisplayName("문자열 날짜에서 한글과 양 옆 공백을 제거한다.")
+//    @Test
+//    void removeKorean() {
+//        System.out.print(DateTimeCustomParser.parseClosingTime("2020.05.19~2020.08.31"));
+//        assertThat(DateTimeCustomParser.parseClosingTime("2020년 6월 6일 까지")).isEqualTo("2020-06-06 00:00:00");
+//    }
+//}
--- dev-timeline-batch/src/test/java/com/sky7th/devtimeline/service/utils/utilTest.java
+++ dev-timeline-batch/src/test/java/com/sky7th/devtimeline/service/utils/utilTest.java
@@ -1,67 +1,67 @@
-package com.sky7th.devtimeline.service.utils;
-
-import com.sky7th.devtimeline.batch.dto.CrawlingDto;
-import com.sky7th.devtimeline.core.domain.recruitpost.domain.RecruitPostRepository;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class utilTest {
-
-    @Autowired
-    RecruitPostRepository recruitPostRepository;
-
-    @Test
-    void test() {
-        String contentUrl = "https://recruit.navercorp.com/naver/job/detail/developer?annoId=20003097&classId=&jobId=&entTypeCd=&searchTxt=&searchSysComCd=";
-        Pattern p = Pattern.compile("(?<=annoId=).+(?=&classId)");
-        Matcher matcher = p.matcher(contentUrl);
-        if (matcher.find()) {
-            System.out.println(matcher.group());
-        }
-    }
-
-    @Test
-    void test2() {
-        String contentUrl = "https://tech.kakao.com/2018/09/12/code-festival-2018-round-2/";
-        Pattern p = Pattern.compile("(?<=com/).+/.+/.+/.+(?=/)");
-        Matcher matcher = p.matcher(contentUrl);
-        if (matcher.find()) {
-            System.out.println(matcher.group());
-        }
-    }
-
-    @Test
-    void test3() {
-        UUID id1 = UUID.nameUUIDFromBytes("NAVER-RECRUIT-20004178".getBytes());
-        UUID id2 = UUID.nameUUIDFromBytes("NAVER-RECRUIT-20004178".getBytes());
-
-        System.out.println(id1.toString());
-        System.out.println(id2.toString());
-    }
-
-    @Test
-    void test4() {
-        CrawlingDto crawlingDto = CrawlingDto.builder()
-                .crawlId("NAVER-RECRUIT-20004178")
-                .author("lee")
-                .closingDate("")
-                .thumbnailUrl("")
-                .title("title")
-                .description("desc")
-                .contentUrl("http")
-                .build();
-//        recruitPostRepository.save(crawlingDto.toRecruitPost());
-//        RecruitPost recruitPost = recruitPostRepository.findById(1L).orElse(null);
-//        assert recruitPost != null;
-//        System.out.println(recruitPost.getPost().getCrawlId());
-    }
-}
+//package com.sky7th.devtimeline.service.utils;
+//
+//import com.sky7th.devtimeline.batch.dto.CrawlingDto;
+//import com.sky7th.devtimeline.core.domain.recruitpost.domain.RecruitPostRepository;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.util.UUID;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class utilTest {
+//
+//    @Autowired
+//    RecruitPostRepository recruitPostRepository;
+//
+//    @Test
+//    void test() {
+//        String contentUrl = "https://recruit.navercorp.com/naver/job/detail/developer?annoId=20003097&classId=&jobId=&entTypeCd=&searchTxt=&searchSysComCd=";
+//        Pattern p = Pattern.compile("(?<=annoId=).+(?=&classId)");
+//        Matcher matcher = p.matcher(contentUrl);
+//        if (matcher.find()) {
+//            System.out.println(matcher.group());
+//        }
+//    }
+//
+//    @Test
+//    void test2() {
+//        String contentUrl = "https://tech.kakao.com/2018/09/12/code-festival-2018-round-2/";
+//        Pattern p = Pattern.compile("(?<=com/).+/.+/.+/.+(?=/)");
+//        Matcher matcher = p.matcher(contentUrl);
+//        if (matcher.find()) {
+//            System.out.println(matcher.group());
+//        }
+//    }
+//
+//    @Test
+//    void test3() {
+//        UUID id1 = UUID.nameUUIDFromBytes("NAVER-RECRUIT-20004178".getBytes());
+//        UUID id2 = UUID.nameUUIDFromBytes("NAVER-RECRUIT-20004178".getBytes());
+//
+//        System.out.println(id1.toString());
+//        System.out.println(id2.toString());
+//    }
+//
+//    @Test
+//    void test4() {
+//        CrawlingDto crawlingDto = CrawlingDto.builder()
+//                .crawlId("NAVER-RECRUIT-20004178")
+//                .author("lee")
+//                .closingDate("")
+//                .thumbnailUrl("")
+//                .title("title")
+//                .description("desc")
+//                .contentUrl("http")
+//                .build();
+////        recruitPostRepository.save(crawlingDto.toRecruitPost());
+////        RecruitPost recruitPost = recruitPostRepository.findById(1L).orElse(null);
+////        assert recruitPost != null;
+////        System.out.println(recruitPost.getPost().getCrawlId());
+//    }
+//}
