7fa75d96e229c624ed6d5ca81c3f4672633cab5b
Taehwa Lee <xoghk0321@gmail.com>
2020-09-29 23:49:13 +0900

    feat: 기존에 CompanyUrl를 통해 Company를 가져오던 로직을 Company를 바로 가져오도록 변경

--- dev-timeline-core/src/main/java/com/sky7th/devtimeline/core/domain/company/domain/CompanyUrlRepository.java
+++ dev-timeline-core/src/main/java/com/sky7th/devtimeline/core/domain/company/domain/CompanyUrlRepository.java
@@ -1,13 +1,8 @@
 package com.sky7th.devtimeline.core.domain.company.domain;
 
-import java.util.List;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyUrlRepository extends JpaRepository<CompanyUrl,Long> {
-
-  @EntityGraph(attributePaths = "company")
-  List<CompanyUrl> findAllByCompanyUrlType(CompanyUrlType companyUrlType);
 }
