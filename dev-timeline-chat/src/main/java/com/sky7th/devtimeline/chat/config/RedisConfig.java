59abfb729368ad6f4b960c5770540b1a9d03cbb3
Taehwa Lee <xoghk0321@gmail.com>
2020-10-02 23:29:47 +0900

    feat: redis publisher, subscriber

--- dev-timeline-chat/src/main/java/com/sky7th/devtimeline/chat/config/RedisConfig.java
+++ dev-timeline-chat/src/main/java/com/sky7th/devtimeline/chat/config/RedisConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -13,33 +12,25 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
     @Bean
-    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory,
-                                                              MessageListenerAdapter listenerAdapter,
-                                                              ChannelTopic channelTopic) {
+    MessageListenerAdapter messageListener(RedisSubscriber redisSubscriber) {
+        return new MessageListenerAdapter(redisSubscriber);
+    }
+
+    @Bean
+    public RedisMessageListenerContainer RedisMessageListener(RedisConnectionFactory connectionFactory) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(listenerAdapter, channelTopic);
         return container;
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplateForObject(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
         return redisTemplate;
     }
-
-    @Bean
-    public ChannelTopic channelTopic() {
-        return new ChannelTopic("chatroom");
-    }
-
-    @Bean
-    public MessageListenerAdapter listenerAdapter(RedisSubscriber subscriber) {
-        return new MessageListenerAdapter(subscriber, "sendMessage");
-    }
-
 }
--- dev-timeline-chat/src/main/java/com/sky7th/devtimeline/chat/pubsub/RedisSubscriber.java
+++ dev-timeline-chat/src/main/java/com/sky7th/devtimeline/chat/pubsub/RedisSubscriber.java
@@ -4,23 +4,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sky7th.devtimeline.chat.model.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
-public class RedisSubscriber {
+public class RedisSubscriber implements MessageListener {
 
     private final ObjectMapper objectMapper;
+    private final RedisTemplate<String, Object> redisTemplate;
     private final SimpMessageSendingOperations messagingTemplate;
 
-    public void sendMessage(String publishMessage) {
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
         try {
-            ChatMessage chatMessage = objectMapper.readValue(publishMessage, ChatMessage.class);
-            messagingTemplate.convertAndSend("/sub/chat/room/" + chatMessage.getRoomId(), chatMessage);
+            String body = redisTemplate.getStringSerializer().deserialize(message.getBody());
+            ChatMessage chatMessage = objectMapper.readValue(body, ChatMessage.class);
+            messagingTemplate.convertAndSend("/sub/chat/rooms/" + chatMessage.getRoomId(), chatMessage);
         } catch (Exception e) {
-            log.error("Exception {}", e);
+            log.error(e.getMessage());
         }
     }
 }
